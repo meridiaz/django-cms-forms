294b73be296af6ae4659aef528b44e6ddc61bb1a
meri <mdiaz>
2020-04-26 00:49:20 +0200

    mejorando programa con pep8

--- cms/views.py
+++ cms/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth import logout
 from .forms import ContenidoForm, ComentarioForm
 from .models import Contenido, Comentario
 
+
 @csrf_exempt
 def get_content(request, llave):
     if request.method == "PUT":
@@ -16,7 +17,8 @@ def get_content(request, llave):
         action = request.POST['action']
         if action == "Enviar Contenido":
             valor = request.POST['valor']
-    if request.method == "PUT" or (request.method == "POST" and action == "Enviar Contenido"):
+    if request.method == "PUT" or
+    (request.method == "POST" and action == "Enviar Contenido"):
         try:
             c = Contenido.objects.get(clave=llave)
             c.valor = valor
@@ -24,11 +26,12 @@ def get_content(request, llave):
             c = Contenido(clave=llave, valor=valor)
         c.save()
     if request.method == "POST" and action == "Enviar Comentario":
-            c = get_object_or_404(Contenido, clave=llave)
-            titulo = request.POST['titulo']
-            cuerpo = request.POST['cuerpo']
-            q = Comentario(contenido=c, titulo=titulo, cuerpo=cuerpo, fecha=timezone.now())
-            q.save()
+        c = get_object_or_404(Contenido, clave=llave)
+        titulo = request.POST['titulo']
+        cuerpo = request.POST['cuerpo']
+        q = Comentario(contenido=c, titulo=titulo, cuerpo=cuerpo,
+                        fecha=timezone.now())
+        q.save()
 
     contenido = get_object_or_404(Contenido, clave=llave)
     context = {'contenido': contenido}
@@ -40,6 +43,7 @@ def index(request):
     context = {'content_list': content_list}
     return render(request, 'cms/index.html', context)
 
+
 def loggedIn(request):
     if request.user.is_authenticated:
         logged = "Logged in as " + request.user.username
@@ -47,14 +51,17 @@ def loggedIn(request):
         logged = "Not logged in. <a href='/admin/'>Login via admin</a>"
     return HttpResponse(logged)
 
+
 def logout_view(request):
     logout(request)
     return redirect("/cms/")
+
 
 def imagen(request):
     template = loader.get_template('cms/plantilla.html')
     context = {}
     return HttpResponse(template.render(context, request))
+
 
 def cms_new(request):
     if request.method == "POST":
@@ -66,6 +73,7 @@ def cms_new(request):
         form = ContenidoForm()
     return render(request, 'cms/cms_edit.html', {'form': form})
 
+
 def cms_modify(request, llave):
     contenido = get_object_or_404(Contenido, clave=llave)
     if request.method == "POST":
@@ -74,9 +82,10 @@ def cms_modify(request, llave):
             contenido = form.save()
             return redirect('get_content', llave=contenido.clave)
     else:
-        #muestra el formulario con los datos que ya tiene contenido
+        # muestra el formulario con los datos que ya tiene contenido
         form = ContenidoForm(instance=contenido)
     return render(request, 'cms/cms_edit.html', {'form': form})
+
 
 def comentario_modify(request, llave, titulo_comen):
     contenido = get_object_or_404(Contenido, clave=llave)
@@ -90,9 +99,10 @@ def comentario_modify(request, llave, titulo_comen):
 
             return redirect('get_content', llave=contenido.clave)
     else:
-        #muestra el formulario con los datos que ya tiene contenido
+        # muestra el formulario con los datos que ya tiene contenido
         form = ComentarioForm(instance=comentario)
     return render(request, 'cms/comentario_edit.html', {'form': form})
+
 
 def comentario_new(request, llave):
     contenido = get_object_or_404(Contenido, clave=llave)
@@ -101,7 +111,8 @@ def comentario_new(request, llave):
         if form.is_valid():
             comentario = form.save(commit=False)
             comentario.fecha = timezone.now()
-            contenido.comentario_set.create(titulo=comentario.titulo, cuerpo=comentario.cuerpo, fecha=comentario.fecha)
+            contenido.comentario_set.create(titulo=comentario.titulo,
+                            cuerpo=comentario.cuerpo, fecha=comentario.fecha)
 
             return redirect('get_content', llave=contenido.clave)
     else:
